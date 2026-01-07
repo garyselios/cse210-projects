f2bb31d91ab12775171a563c7f4f6bc060e615a9
garyselios <garyseliosbennett@hotmail.com>
2026-01-07 18:50:03 -0300

    test 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,21 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // 1. Ask for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // 2. ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // 3. Show "last name, fist name last name"
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+
