924a1f2514a95a9ad663de62289897a703490cb6
garyselios <garyseliosbennett@hotmail.com>
2026-01-12 17:07:41 -0300

    week2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create jobs
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create and show resume
+        Resume myResume = new Resume();
+        myResume._personName = "Gary Selios";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
